c6b4962bacc1f0ac83f4f83881d70c022396f200
kashidislam <kashid.islam1412@gmail.com>
2023-11-24 18:37:38 +0530

    Adding the recursive code for array reversal

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -1,9 +1,13 @@
 public class ReverseArray{
     public static void main(String [] args){
-        int [] arr = {1,2,3,4,5,6};
+        int [] arr = {1,2,3,4,5};
         printArray(arr);
-        reverseArray(arr);
+       // reverseArray(arr);
+       // printArray(arr);
+        System.out.println("REVERSE AN ARRAY RECURSIVELY");
+        reverseArray(arr, 0, arr.length-1);
         printArray(arr);
+
     }
 
     public static void reverseArray(int [] a){
@@ -22,5 +26,16 @@ public class ReverseArray{
             System.out.print(val + " ");
         }
         System.out.println();
+    }
+
+    // REVERSE AN ARRAY USING RECURSION
+
+    public static void reverseArray(int [] a, int s, int e){
+        if(s < e){
+            int temp = a[s];
+            a[s] = a[e];
+            a[e] = temp;
+            reverseArray(a,s+1,e-1);
+        }
     }
 }
